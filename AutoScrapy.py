f6de65b4420d7ec2f4739693ce51e8cc9453514c
yub168 <23651016@qq.com>
2023-12-18 14:12:30 +0800

    update sites

--- AutoScrapy.py
+++ AutoScrapy.py
@@ -6,6 +6,7 @@ import json5
 list={
   'fatCat':'http://肥猫.live',
   'YourSmile':'https://agit.ai/Yoursmile7/TVBox/raw/branch/master/XC.json',
+  '道长':'https://pastebin.com/raw/5NHaxyGR',
   '香雅情':'https://ghproxy.net/https://raw.githubusercontent.com/xyq254245/xyqonlinerule/main/XYQTVBox.json'
   }
 configList={}
@@ -21,26 +22,42 @@ for key,value in list.items():
     configList[key]=json5.loads(r.text)
     
 
-#配置主体内容
+# 配置主体内容
 if 'fatCat' in configList:
   customConfig=configList['fatCat']
 elif 'YourSmile' in configList:
   customConfig=configList['YourSmile']
 elif '香雅情' in configList:
   customConfig=configList['香雅情']
+
+# 给主体添加部分site
+if '道长' in configList:
+  # 添加农民site
+  sites=configList['道长']['sites']
+  for site in sites:
+    if '农民' in site['key']:
+      customConfig['sites'].append(site)
+    if '秋霞影院' in site['key']:
+      customConfig['sites'].append(site)
+    if '奇优影院' in site['key']:
+      customConfig['sites'].append(site)
+    if '米爱影视' in site['key']:
+      customConfig['sites'].append(site)
+  
+
 if customConfig :
-  #提取解析parses
-  #parses=[]
+
+  # 提取解析parses
   if '香雅情' in configList:
     parses=configList['香雅情']['parses']
     customConfig['parses']=parses
-  #提取lives
-  #lives=[]
+
+  # 提取lives
   if 'YourSmile' in configList:
     lives=configList['YourSmile']['lives']
     customConfig['lives']=lives
 
-  #配置customConfig及写入文件
+  # 配置customConfig及写入文件
   with open("./config.json", "w",encoding='utf-8') as file:
     # 使用json.dump将数据写入文件
     print(customConfig)
