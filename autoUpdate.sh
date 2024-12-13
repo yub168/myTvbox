ed8dbf6be1e2e0d871193da9121f8de5987842b7
yub168 <23651016@qq.com>
2024-12-13 15:28:45 +0800

    update autoup

--- autoUpdate.sh
+++ autoUpdate.sh
@@ -3,7 +3,7 @@ cd /home/yub168/myTvbox
 git fetch github
 git reset --hard github/master
 chmod -R 755 /home/yub168/myTvbox
-python3 AutoScrapy.py
+venv/bin/python AutoScrapy.py
 git add .
 git commit -m 'update'
 git push github master
